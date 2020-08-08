fd8658403cd1333573a2a29e0325043a32499574
stosikmartyna <stosik.martyna@gmail.com>
2020-08-08 19:19:55 +0200

    Create table with data from API.

--- src/Components/CountriesView.jsx
+++ src/Components/CountriesView.jsx
@@ -1,25 +1,53 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './countriesView.css';
 
 export const CountriesView = () => {
-  const [countryData, setCountryData] = useState([]);
+    const [countries, setCountries] = useState(undefined);
 
-  const fetchCountryData = async() => {   
-    try {
-        const response = await axios.get('https://restcountries.eu/rest/v2/all')
-        setCountryData(response.data)
-    } catch (error) {
-        console.error(error)
-    } 
-  }
+    const fetchCountryData = async () => {
+        try {
+            const response = await axios.get('https://restcountries.eu/rest/v2/all')
+            setCountries(response.data)
+        } catch (error) {
+            console.error(error)
+        }
+    }
 
-  useEffect(() => {
-    fetchCountryData();
-}, []);
+    useEffect(() => {
+        fetchCountryData();
+    }, []);
 
-  return (
-    <>
-      {countryData.map(country => <li>{country.name}</li>)}
-    </>
-  ); 
-}
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Flag</th>
+                    <th>Country Name</th>
+                    <th>Native Name</th>
+                    <th>Capital</th>
+                    <th>Region</th>
+                    <th>Subregion</th>
+                    <th>Population</th>
+                    <th>Area</th>
+                </tr>
+            </thead>
+            <tbody>
+                {countries?.map(country => {
+                        return (
+                            <tr>
+                                <td><img src={country.flag} /></td>
+                                <td>{country.name}</td>
+                                <td>{country.nativeName}</td>
+                                <td>{country.capital}</td>
+                                <td>{country.region}</td>
+                                <td>{country.subregion}</td>
+                                <td>{country.population}</td>
+                                <td>{country.area}</td>
+                            </tr>
+                        )
+                    })
+                }
+            </tbody>
+        </table>
+    )}
