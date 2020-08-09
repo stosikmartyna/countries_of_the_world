b09f81669ab4b0d6979f84c8bd6c96bd4406bb13
stosikmartyna <stosik.martyna@gmail.com>
2020-08-09 17:12:24 +0200

    Extract CountriesTable to separated component.

--- src/Components/CountriesList/CountriesList.jsx
+++ src/Components/CountriesList/CountriesList.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Header } from '../Header/Header';
 import { SearchInput } from '../SearchInput/SearchInput';
-import './CountriesList.css';
+import { CountriesTable } from '../CountriesTable/CountriesTable';
 
 export const CountriesList = () => {
     const [countries, setCountries] = useState(undefined);
@@ -37,36 +37,7 @@ export const CountriesList = () => {
         <>
             <Header />
             <SearchInput filterCountries={filterCountries} />
-            <table>
-                <thead>
-                    <tr>
-                        <th>Flag</th>
-                        <th>Country Name</th>
-                        <th>Native Name</th>
-                        <th>Capital</th>
-                        <th>Region</th>
-                        <th>Subregion</th>
-                        <th>Population</th>
-                        <th>Area</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {filteredCountries?.map(country => {
-                        return (
-                            <tr key={country.name}>
-                                <td><img src={country.flag} alt='flag' /></td>
-                                <td>{country.name}</td>
-                                <td>{country.nativeName}</td>
-                                <td>{country.capital}</td>
-                                <td>{country.region}</td>
-                                <td>{country.subregion}</td>
-                                <td>{country.population}</td>
-                                <td>{country.area}</td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
+            <CountriesTable filteredCountries={filteredCountries} />
         </>
     )
 }
---
+++ src/Components/CountriesTable/CountriesTable.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './CountriesTable.css';
+
+export const CountriesTable = ({filteredCountries}) => {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Flag</th>
+                    <th>Country Name</th>
+                    <th>Native Name</th>
+                    <th>Capital</th>
+                    <th>Region</th>
+                    <th>Subregion</th>
+                    <th>Population</th>
+                    <th>Area (km²)</th>
+                </tr>
+            </thead>
+            <tbody>
+                {filteredCountries?.map(country => {
+                    return (
+                        <tr key={country.name}>
+                            <td className={'flag-cell'}>
+                                <img src={country.flag} alt='flag' />
+                            </td>
+                            <td>{country.name}</td>
+                            <td>{country.nativeName}</td>
+                            <td>{country.capital}</td>
+                            <td>{country.region}</td>
+                            <td>{country.subregion}</td>
+                            <td className={'amount-cell'}>{country.population}</td>
+                            <td className={'amount-cell'}>{country.area}</td>
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table>
+    )
+}
