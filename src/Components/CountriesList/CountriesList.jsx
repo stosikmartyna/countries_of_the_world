f5f6cc39dc17686e04e115ae4d2a253663e19c23
stosikmartyna <stosik.martyna@gmail.com>
2020-08-11 12:42:55 +0200

    Create scroll to top button.

--- src/Components/CountriesList/CountriesList.jsx
+++ src/Components/CountriesList/CountriesList.jsx
@@ -9,6 +9,7 @@ import './CountriesList.css';
 export const CountriesList = () => {
     const [countries, setCountries] = useState(undefined);
     const [filteredCountries, setFilteredCountries] = useState(undefined);
+    const [showScroll, setShowScroll] = useState(false)
 
     const regions = countries?.map(country => country.region).filter((region, index, regions) => regions.indexOf(region) === index).filter(region => region !== '');
 
@@ -46,6 +47,21 @@ export const CountriesList = () => {
         fetchCountryData();
     }, []);
 
+    const checkScrollTop = () => {
+        if (!showScroll && window.pageYOffset > 200){
+          setShowScroll(true)
+        } else if (showScroll && window.pageYOffset <= 200){
+          setShowScroll(false)
+        }
+      };
+    
+      const scrollTop = () => {
+        window.scrollTo({top: 0, behavior: 'smooth'});
+      };
+    
+      window.addEventListener('scroll', checkScrollTop)
+
+
     return (
         <>
             <Header />
@@ -54,7 +70,9 @@ export const CountriesList = () => {
                 <SelectRegion onSelectChange={handleSelectChange} regions={regions} />
             </div>
             <CountriesTable filteredCountries={filteredCountries} />
+            <button className="scrollTop" onClick={scrollTop} style={{display: showScroll ? 'flex' : 'none'}}> 
+                <img src="scrollTopButton.png"/>
+            </button>
         </>
     )
 }
-
