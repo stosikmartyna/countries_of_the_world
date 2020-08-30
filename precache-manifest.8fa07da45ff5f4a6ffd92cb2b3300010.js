da31f45d0b2ec3b6066b5ac1eb0381dfca7f50c1
stosikmartyna <stosik.martyna@gmail.com>
2020-08-30 13:45:41 +0200

    Updates

---
+++ precache-manifest.8fa07da45ff5f4a6ffd92cb2b3300010.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7ea670fb28ff3b5ae317b818049140d",
+    "url": "/world_countries/index.html"
+  },
+  {
+    "revision": "713aacf06b6443d28b30",
+    "url": "/world_countries/static/css/main.1109be89.chunk.css"
+  },
+  {
+    "revision": "2516b877d85cfe418c41",
+    "url": "/world_countries/static/js/2.9898e8f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/world_countries/static/js/2.9898e8f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "713aacf06b6443d28b30",
+    "url": "/world_countries/static/js/main.08025ac6.chunk.js"
+  },
+  {
+    "revision": "9a2c1b159b08e466b54d",
+    "url": "/world_countries/static/js/runtime-main.50908616.js"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/world_countries/static/media/Roboto-Regular.11eabca2.ttf"
+  }
+]);
